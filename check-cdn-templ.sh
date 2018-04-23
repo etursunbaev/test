6a69197c977454edfcd0182fc8b3daab690d0344
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-04-23 14:38:13 +0600

    fixing

--- check-cdn-templ.sh
+++ check-cdn-templ.sh
@@ -1,2 +1,5 @@
-curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed `'s,"id": ",,g'` | sed `'s,",,g'` | sed 's/,//g' > /tmp/abc
+#!/bin/bash
+
+set -e
+curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed 's,"id": ",,g' | sed 's,",,g' | sed 's/,//g' > /tmp/abc
 cat /tmp/abc
