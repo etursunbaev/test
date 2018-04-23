64dd849be20d28d4215f92d5978f201eeaf4e275
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-04-23 14:49:14 +0600

    extending installation

--- check-cdn-templ.sh
+++ check-cdn-templ.sh
@@ -3,3 +3,19 @@
 set -e
 curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed 's,"id": ",,g' | sed 's,",,g' | sed s/,//g > /tmp/abc
 cat /tmp/abc
+
+echo "" > /tmp/error.log
+echo "" > /tmp/success.log
+ID=(cat /tmp/abc)
+declare -a arr=($ID)
+for i in "${arr[@]}"
+do
+out=`sudo subutai import id:$i`
+if [ "$?" == 0 ]; then
+       echo "$i - success $out" >> /tmp/success.log
+else
+       echo "$i - failed $out" >> /tmp/error.log
+fi
+template=`sudo subutai list -t | sed -n '3p'`
+sudo subutai destroy $template
+done
