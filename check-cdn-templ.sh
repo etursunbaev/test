04c3be45e49faaa1f516fc120af4e58cc8706005
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-04-23 15:01:04 +0600

    excluding sudo

--- check-cdn-templ.sh
+++ check-cdn-templ.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo -i
 
 set -e
 curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed 's,"id": ",,g' | sed 's,",,g' | sed s/,//g > /tmp/abc
@@ -10,12 +11,12 @@ ID=(cat /tmp/abc)
 declare -a arr=($ID)
 for i in "${arr[@]}"
 do
-out=`sudo subutai-master import id:$i`
+out=`subutai-master import id:$i`
 if [ "$?" == 0 ]; then
        echo "$i - success $out" >> /tmp/success.log
 else
        echo "$i - failed $out" >> /tmp/error.log
 fi
-template=`sudo subutai-master list -t | sed -n '3p'`
-sudo subutai-master destroy $template
+template=`subutai-master list -t | sed -n '3p'`
+subutai-master destroy $template
 done
