9568ec4f15425ede576087a712ecf740f711cd1f
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-04-23 14:28:12 +0600

    fixing syntax

--- check-cdn-templ.sh
+++ check-cdn-templ.sh
@@ -1,2 +1,2 @@
-curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed `'s,"id": ",,g'` | sed `'s,",,g'` | sed `'s/.\$//'` > /tmp/abc
+curl https://mastercdn.subutai.io:8338/kurjun/rest/template/list | python -m json.tool | grep id | sed `'s,"id": ",,g'` | sed `'s,",,g'` | sed 's/,//g' > /tmp/abc
 cat /tmp/abc
